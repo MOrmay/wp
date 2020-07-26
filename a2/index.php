3511f16127f8fcccaafc209b71dc66b7a1f15854
MOrmay <s3795048@student.rmit.edu.au>
2020-07-26 23:46:29 +0930

    top nav done

--- a2/index.php
+++ a2/index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Home - Douglas Raymond Baker</title>
     
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
@@ -14,41 +15,37 @@
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
+      <div class="title">ANZAC Douglas Raymond Baker - Letters Home<!DOCTYPE html></div>
     </header>
 
+    <!--I had some trouble implementing an active selection for the nav bar so I opted for a
+        fixed title at the top of the page-->
     <nav class="topNav" id="myTopNav">
-      
-        <a href="index.php">Home</a>
-        <a href="index.php">About Douglas</a>
-        <a href="index.php">Letters</a>
-        <a href="index.php">Postcards</a>
-        <div class="dropdown">
-          <button class="dropbtn">Events
-            <i class="fa fa-claret-down"></i>
-          </button>
-          <div class="dropdown-content">
-            <a href="index.php">Gallipoli</a>
-            <a href="index.php">The Big Push</a>
-            <a href="index.php">Battle Of Poziers</a>
-          </div>
+      <a href="index.php">Home</a>
+      <a href="about_Douglas.html">About Douglas</a>
+      <a href="index.php">Letters</a>
+      <a href="index.php">Postcards</a>
+      <div class="dropdown">
+        <button class="dropbtn">Events &#9660
+          <i class="fa fa-claret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="index.php">Gallipoli</a>
+          <a href="index.php">The Big Push</a>
+          <a href="index.php">Battle Of Poziers</a>
         </div>
-        <a href="index.php">Places</a>
-        <div class="dropdown">
-          <button class="dropbtn">Records
-            <i class="fa fa-claret-down"></i>
-          </button>
-          <div class="dropdown-content">
-            <a href="https://www.aif.adfa.edu.au/showPerson?pid=11163">Service Summary</a>
-            <a href="http://recordsearch.naa.gov.au/scripts/Imagine.asp?B=3009496">Service Record</a>
-           </div>
+      </div>
+      <a href="index.php">Places</a>
+      <div class="dropdown">
+        <button class="dropbtn">Records &#9660
+          <i class="fa fa-claret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="https://www.aif.adfa.edu.au/showPerson?pid=11163">Service Summary</a>
+          <a href="http://recordsearch.naa.gov.au/scripts/Imagine.asp?B=3009496">Service Record</a>
         </div>
-        
-        <a href="javascript:void(0);" class="icon" onclick="menuExpand()">
-              <i class="fa fa-bars"></i>        
-            </a>
-        
-      
+      </div>
+      <a href="javascript:void(0);" class="icon" onclick="menuExpand()">&#9776;</a>
     </nav>
 
     <main>
@@ -56,6 +53,9 @@
     <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
         <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
       </article>
+      <p>
+        Just a bunch of <br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.<br>.
+       </p>
     </main>
 
     <footer>
@@ -66,5 +66,17 @@
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
 
+    <script>
+      function menuExpand() {
+        var x = document.getElementById("myTopNav");
+        if (x.className === "topNav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topNav";
+        }
+      }
+    </script>
+
   </body>
+
 </html>
